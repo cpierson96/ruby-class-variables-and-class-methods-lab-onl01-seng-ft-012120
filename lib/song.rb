f902c62b53a88dfde9390e7b3bc9ccf775ff0989
Claudia Pierson <c172@umbc.edu>
2020-02-08 20:21:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,6 +9,23 @@ class Song
     @name = song_name
     @artist = artist
     @genre = genre 
+    @@count += 1 
+    @@genres << genre
+    @@artists << artist 
+  end 
+  
+  def self.count 
+    @@count
+  end 
+  
+  def self.artists
+    @@artists 
+  end 
+  
+  def self.artists 
+    @@artists
+  end 
+  
 
 
 
